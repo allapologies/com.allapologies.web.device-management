82bcbe03a96dcf90e568a2a7dd6b4d486f697c7a
Aleksandr <aleksandr.panteleev@sixt.com>
2023-08-10 08:33:35 +0200

    dialog

---
+++ src/components/Dialog.tsx
@@ -0,0 +1,27 @@
+import { ReactNode } from 'react';
+import { Dialog as MuiDialog, DialogActions, DialogContent, DialogTitle, } from '@mui/material';
+
+type Props = {
+  title: string;
+  content: ReactNode;
+  actions: ReactNode;
+};
+
+export const Dialog = (props: Props) => {
+  const {
+    title,
+    content,
+    actions,
+  } = props;
+  return (
+    <MuiDialog open maxWidth="sm" fullWidth>
+      <DialogTitle>{title}</DialogTitle>
+      <DialogContent>
+        {content}
+      </DialogContent>
+      <DialogActions>
+        {actions}
+      </DialogActions>
+    </MuiDialog>
+  );
+}
