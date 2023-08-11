78b7d56da4234676eb1287f15ebab0498787e344
Aleksandr <aleksandr.panteleev@sixt.com>
2023-08-11 20:10:47 +0200

    fetch

--- src/service/mocks.ts
+++ src/service/mocks.ts
@@ -4,70 +4,70 @@ export const devices: Device[] = [
   {
     "id": "d1",
     "name": "Smartphone",
-    "type": "Mobile",
+    "type": "smartphone",
     "owner": "John",
     "batteryStatus": 80
   },
   {
     "id": "d2",
     "name": "Laptop",
-    "type": "Computer",
+    "type": "desktop",
     "owner": "Jane",
     "batteryStatus": 60
   },
   {
     "id": "d3",
     "name": "Smartwatch",
-    "type": "Wearable",
+    "type": "smartwatch",
     "owner": "Bob",
     "batteryStatus": 90
   },
   {
     "id": "d4",
     "name": "Tablet",
-    "type": "Mobile",
+    "type": "smartphone",
     "owner": "Alice",
     "batteryStatus": 70
   },
   {
     "id": "d5",
     "name": "Smart TV",
-    "type": "Television",
+    "type": "smartphone",
     "owner": "David",
     "batteryStatus": 100
   },
   {
     "id": "d6",
     "name": "Smart Speaker",
-    "type": "Speaker",
+    "type": "tablet",
     "owner": "Karen",
     "batteryStatus": 50
   },
   {
     "id": "d7",
     "name": "Smart Thermostat",
-    "type": "Thermostat",
+    "type": "tablet",
     "owner": "Mike",
     "batteryStatus": 80
   },
   {
     "id": "d8",
     "name": "Smart Lock",
-    "type": "Lock",
+    "type": "tablet",
     "owner": "Samantha",
     "batteryStatus": 30
   },
   {
     "id": "d9",
     "name": "Smart Refrigerator",
-    "type": "Appliance",
+    "type": "desktop",
     "owner": "Tom",
     "batteryStatus": 40
   },
   {
     "id": "d10",
     "name": "Smart Camera",
-    "type": "Camera",
+    "type": "tablet",
     "owner": "Emily",
     "batteryStatus": 60
   }
